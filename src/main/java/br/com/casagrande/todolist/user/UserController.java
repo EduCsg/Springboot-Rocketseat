6164ffd9dc801a28af59c5a147a304748f276a7a
Eduardo Casagrande <eduardocasagrande71@gmail.com>
2023-10-12 21:13:47 -0300

    Aula 02 - lombok, h2 e jpa

--- src/main/java/br/com/casagrande/todolist/user/UserController.java
+++ src/main/java/br/com/casagrande/todolist/user/UserController.java
@@ -1,16 +1,28 @@
 package br.com.casagrande.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public String create(@RequestBody UserModel userModel) {
-        return "O usuário " + userModel.getName() + " foi criado!";
+    public ResponseEntity<?> create(@RequestBody UserModel userModel) {
+
+        UserModel user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) return ResponseEntity.badRequest().body("User already exists");
+
+        return ResponseEntity.ok(this.userRepository.save(userModel));
     }
 }
